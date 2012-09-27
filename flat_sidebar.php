0184b5757187d1a40d5f4720b497be988b2c7df2
Riccardo Strobbia <riccardo@robanostra.com>
2012-09-27 16:30:32 +0200

    bloginfo

--- flat_sidebar.php
+++ flat_sidebar.php
@@ -6,6 +6,6 @@ if (function_exists('bcn_display'))
 	// Display the breadcrumb
 	bcn_display();
 } echo"</div>";
-echo '<div class="arealog logfl"><a href="http://www.areato.org/" target="_blank"><img alt="" src="http://dito.areato.org/wp-content/themes/area/imgs/arealog.jpg" class="imgLogo"/></a></div>';
+echo '<div class="arealog logfl"><a href="http://www.areato.org/" target="_blank"><img alt="" src="'.get_bloginfo('url').'/wp-content/themes/area/imgs/arealog.jpg" class="imgLogo"/></a></div>';
 ?>
 </div>
