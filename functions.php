14e158d117c277c4ed3b5b75675ae6484501f640
Riccardo Strobbia <riccardo@robanostra.com>
2013-02-17 20:24:53 +0100

    New excerpt

--- functions.php
+++ functions.php
@@ -422,4 +422,28 @@ function dito_getCategoryId()
 		}
 	}
 }
+function pippin_excerpt_by_id($post, $length = 10, $tags = '<a><em><strong>', $extra = ' [Leggi tutto]') {
+ 
+	if(is_int($post)) {
+		// get the post object of the passed ID
+		$post = get_post($post);
+	} elseif(!is_object($post)) {
+		return false;
+	}
+ 
+	if(has_excerpt($post->ID)) {
+		$the_excerpt = $post->post_excerpt;
+		return apply_filters('the_content', $the_excerpt);
+	} else {
+		$the_excerpt = $post->post_content;
+	}
+	
+	$the_excerpt = strip_shortcodes(strip_tags($the_excerpt), $tags);
+	$the_excerpt = preg_split('/\b/', $the_excerpt, $length * 2+1);
+	$excerpt_waste = array_pop($the_excerpt);
+	$the_excerpt = implode($the_excerpt);
+	$the_excerpt .= ' ...<a href="'.get_permalink($post->ID).'">'.$extra.'</a>';
+ 
+	return apply_filters('the_content', $the_excerpt);
+}
 ?>
